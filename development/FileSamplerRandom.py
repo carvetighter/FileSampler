08caa5810390d49e07e69d3cb5d9c0dcc6c9c5f7
Brent Allen <carvetighter@gmail.com>
2017-12-07 00:15:03 -0600

    consolidated code, self._csv_trans() method

--- development/FileSamplerRandom.py
+++ development/FileSamplerRandom.py
@@ -87,19 +87,16 @@ class RandomAccessReader(object):
 
         return int(mean(list_len_100_lines))
 
-    def get_lines(self, line_number, bool_header = False):
+    def get_line(self, m_int_line_number, bool_header = False):
         """
         get the contents of a given line in the file
         :param line_number: 0-indexed line number
         :param bool_header: flag to pull first line
         :return: str
         """
-        lines = []
         with open(self._filepath) as f:
-            if bool_header:
-                f.seek(line_number)
-            else:
-                f.seek(line_number * self._int_avg_len)
+            f.seek(m_int_line_number * self._int_avg_len)
+            if not bool_header:
                 f.readline()
             return f.readline()
 
@@ -120,11 +117,7 @@ class CsvRandomAccessReader(RandomAccessReader):
         self._ignore_bad_lines = kwargs.get('ignore_corrupt', False)
         self.has_header = has_header
         if has_header:
-            dialect = self.MyDialect(self._endline, self._quotechar, self._delimiter)
-            b = StringIO(self.get_lines(0, bool_header = True))
-            r = csv.reader(b, dialect)
-            values = tuple(next(r))
-            self._headers = values
+            self._headers = self._csv_trans(self.get_line(0, bool_header = True))
 
     @property
     def headers(self):
@@ -135,34 +128,40 @@ class CsvRandomAccessReader(RandomAccessReader):
             raise TypeError("Argument 'header_list' must contain an iterable")
         self._headers = tuple(header_list)
 
+    def _csv_trans(self, string_line):
+        dialect = self.MyDialect(self._endline, self._quotechar, self._delimiter)
+        b = StringIO(string_line)
+        r = csv.reader(b, dialect)
+        return tuple(next(r))
+
     def _get_line_values(self, line):
         """
         Splits the csv line into a list of individual values
         :param line: str
         :return: tuple of str
         """
-        dialect = self.MyDialect(self._endline, self._quotechar, self._delimiter)
-        b = StringIO(line)
-        r = csv.reader(b, dialect)
-        values = tuple(next(r))
+        values = self._csv_trans(line)
         if len(self._headers) != len(values):
             if not self._ignore_bad_lines:
                 raise ValueError("Corrupt csv - header and row have different lengths")
             return None
         return values
 
-    def get_line_dicts(self, line_number, bool_headers = False):
+    def get_sample(self, m_int_num_samples, m_bool_headers = False):
         """
         gets the requested line as a dictionary (header values are the keys)
         :param line_number: requested line number, 0-indexed (disregards the header line if present)
         :return: dict
         """
-        if not self._headers:
-            raise ValueError("Headers must be set before requesting a line dictionary")
+        if m_int_num_samples > self._int_num_lines:
+            raise ValueError('Number of samples requested is more than the number of lines in the file')
+
         if self.has_header:
-            line_number += 1
+            list_random_lines = random.sample(range(1, self._int_num_lines), m_int_num_samples)
+        else:
+            list_random_lines = random.sample(range(0, self._int_num_lines), m_int_num_samples)
         lines = []
-        text_line = self.get_lines(line_number)
+        text_line = self.get_line(line_number)
             #vals = self._get_line_values(text_lines[x])
             #if vals is None:
             #    lines.append(dict(zip(self._headers, list(range(len(self._headers))))))
